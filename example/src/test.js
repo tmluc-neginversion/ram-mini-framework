5d783cb740e6854ec7c97c66bcdca57274931a54
tmluc-neginversion <neginversion@tmluc.com.mx>
2025-10-21 09:43:10 -0600

    Update test.js

--- example/src/test.js
+++ example/src/test.js
@@ -1,10 +1,20 @@
+/* © 2025 Ricardo Alonso Mendiola — MIT License */
+
 test_dropdown = ['PEN','PINEAPPLE','APPLE','PEN'];
 
 async function testOnLoad () {
   const tmp_header_id = 'head-template';
   const tmp_row_id = 'row-template';
 
-  fillSelect(document.getElementById('ff-select-1'),test_dropdown);
+  fillSelect(document.getElementById('ff-select-1'),test_dropdown, true);
+
+  const templateA = document.getElementById('row-template');
+
+  fillSelect(templateA.content.querySelector('.ts-1'),['A','B','C']);
+  fillSelect(templateA.content.querySelector('.ts-2'),['D','E']);
+  fillSelect(templateA.content.querySelector('.ts-3'),['F','G']);
+
+  clearSelects(templateA.content.querySelectorAll('.ts-2, .ts-3'), '');
 
   addHeader('tb-head', tmp_header_id);
   addRow('tb-body', tmp_row_id);
